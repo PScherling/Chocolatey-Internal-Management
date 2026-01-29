4c82a82c3f994fe4ff7e20fb1ae0e495966ec3de
Patrick Scherling <scherling.pa@gmail.com>
2026-01-29 16:52:29 +0100

    Update C4B-OfflineInstall.ps1

--- C4B-OfflineInstall.ps1
+++ C4B-OfflineInstall.ps1
@@ -72,18 +72,18 @@ param(
 
             $OpenFileDialog.filename
         }
-    ),                                                                                      # e.g. D:\License.xml
-    #[Parameter(Mandatory)][string]$ServerFqdn,                                             # e.g. psc-c4bsrv.local
-    [Parameter(Mandatory)][string]$BusinessLicenseGuid,                                     # e.g. You get this from the chocolatey.license.xml file
-    [ValidateSet('http','https')][string]$Protocol = "http",                                # e.g. Default = "http"
-    [Parameter(Mandatory)][string]$ProGetSrv,                                               # e.g. "PSC-SWREPO1"
-    [Parameter(Mandatory = $false)][string] $ProGetPort = "8624",                           # e.g. Default = "8624"
-    [Parameter(Mandatory)][string]$FeedName,                                                # e.g. "choco-internal"
-    [Parameter(Mandatory)][string]$ProGetFeedKey,                                           # e.g. [Your-ProGet-Feed-API-Key] (Not to the Assets!)
-    [Parameter(Mandatory)][string]$DBUser,                                                  # e.g. DB User Name | Default is 'ChocoUser'
-    [Parameter(Mandatory)][string]$DBUserPassword,                                          # e.g. Provide a super hard password!
-    [Parameter(Mandatory)][string]$LocalAdmin,                                              # e.g. Local Windows Admin | Default is 'sysadmineuro'
-    [Parameter][switch]$UseADLoginforDB                                                     # e.g. Use this Switch Parameter if you want to enable DB Login with AD Credentials
+    ),                                                                                        # e.g. D:\License.xml
+    #[Parameter(Mandatory)][string]$ServerFqdn,                                               # e.g. psc-c4bsrv.local
+    [Parameter(Mandatory)] [string] $BusinessLicenseGuid,                                     # e.g. You get this from the chocolatey.license.xml file
+    [ValidateSet('http','https')] [string] $Protocol = "http",                                # e.g. Default = "http"
+    [Parameter(Mandatory)] [string] $ProGetSrv,                                               # e.g. "PSC-SWREPO1"
+    [Parameter(Mandatory = $false)] [string] $ProGetPort = "8624",                            # e.g. Default = "8624"
+    [Parameter(Mandatory)] [string] $FeedName,                                                # e.g. "choco-internal"
+    [Parameter(Mandatory)] [string] $ProGetFeedKey,                                           # e.g. [Your-ProGet-Feed-API-Key] (Not to the Assets!)
+    [Parameter(Mandatory)] [string] $DBUser,                                                  # e.g. DB User Name | Default is 'ChocoUser'
+    [Parameter(Mandatory)] [string] $DBUserPassword,                                          # e.g. Provide a super hard password!
+    [Parameter(Mandatory)] [string] $LocalAdmin,                                              # e.g. Local Windows Admin | Default is 'sysadmineuro'
+    [Parameter] [switch] $UseADLoginforDB                                                     # e.g. Use this Switch Parameter if you want to enable DB Login with AD Credentials
     
 )
 
